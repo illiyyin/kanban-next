e161212e7766fb0e024c5bf52ef64ca737cf2103
illiyyin <iilliyyin@gmail.com>
2024-08-27 00:22:08 +0700

    test click sidebar

--- src/components/__tests__/SideMenu.test.tsx
+++ src/components/__tests__/SideMenu.test.tsx
@@ -1,23 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import SideMenu from '../SideMenu';
+
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 
 describe('SideMenu', () => {
   beforeEach(() => {
-    render(<SideMenu />)
+    render(
+      <SideMenu />,
+      { wrapper: MemoryRouterProvider }
+    );
   })
 
-  it('renders menu Home', () => {
+  it('renders menu Home and redirect to the Home Page', () => {
     const menuLink = screen.getByText("Home")
     expect(menuLink).toBeInTheDocument()
+
+    fireEvent.click(menuLink);
+    expect(mockRouter.asPath).toEqual('/')
   })
 
-  it('renders menu Task List', () => {
+  it('renders menu Task List and redirect to the Task List Page ', () => {
     const menuLink = screen.getByText("Task List")
     expect(menuLink).toBeInTheDocument()
+
+    fireEvent.click(menuLink);
+    expect(mockRouter.asPath).toEqual('/task-list')
   })
 
-  it('renders menu Task Progress', () => {
+  it('renders menu Task Progress and redirect to the Task Progress Page', () => {
     const menuLink = screen.getByText("Task Progress")
     expect(menuLink).toBeInTheDocument()
+
+    fireEvent.click(menuLink);
+    expect(mockRouter.asPath).toEqual('/task-progress')
   })
 });
