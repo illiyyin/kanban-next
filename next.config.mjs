99d70794d2a9cc9be58e6983f4141161e51bb876
illiyyin <iilliyyin@gmail.com>
2025-03-04 10:19:42 +0700

    add config for cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 const nextConfig = {
   output: 'export',
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
 
 export default nextConfig
